4a37a1baac8bea233bfc672af0ca61be3c9bc493
qaraluch <prezenciq@gmail.com>
2018-01-05 10:11:57 +0100

    Migrate to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,8 @@
 const gulp = require("gulp");
 
-// const plugins = require("gulp-load-plugins")();
-// [jackfranklin/gulp-load-plugins: Automatically load in gulp plugins](https://github.com/jackfranklin/gulp-load-plugins)
-// npm i gulp-load-plugins -S
-
 const rename = require("gulp-rename");
 // [hparra/gulp-rename: Rename files easily](https://github.com/hparra/gulp-rename)
 // npm i gulp-rename -S
-
-const filter = require("gulp-filter");
-// [sindresorhus/gulp-filter: Filter files in a vinyl stream](https://github.com/sindresorhus/gulp-filter)
-// npm i gulp-filter -S
 
 const del = require("del");
 // https://github.com/sindresorhus/del
@@ -22,76 +14,31 @@ const debug = require("gulp-debug");
 
 const paths = {};
 
-paths.cu = ".fotos/1_camera_upload/**/*";
-paths.basicFilter = ".fotos/2_basic_filter/**/*";
-paths.outputToBasicFilter = ".fotos/2_basic_filter/";
-paths.outputToWrong = ".fotos/3_wrong/";
-paths.clearBasic = ".fotos/2_basic_filter/*";
-paths.clearWrong = ".fotos/3_wrong/*";
+paths.cu = ".fotos/cu/**/*";
+paths.cuBackup = ".fotos/cuBackup/";
 
-// not deal with streams
-const tasks = {};
+const cleanDir = path => del(path);
+const cleanDir_cuBackup = () => cleanDir(paths.cuBackup);
 
-tasks.cleanDir = function(path) {
-  return del(path);
-};
-
-tasks.clearWrong = async function() {
-  await tasks.cleanDir(paths.clearWrong);
-};
-
-tasks.clearBasic = async function() {
-  await tasks.cleanDir(paths.clearBasic);
-};
-
-tasks.clearAll = async function() {
-  await tasks.cleanDir(paths.clearWrong);
-  await tasks.cleanDir(paths.clearBasic);
-};
-
-// stream tasks
-const pipes = {};
-
-pipes.renameExt = function(from, to) {
-  return rename(path => {
+const renameExt = (from, to) =>
+  rename(path => {
     // path.dirname += "/ciao";
     // path.basename += "-goodbye";
     path.extname = path.extname === from ? to : path.extname;
   });
+
+const pipes = {};
+
+pipes.rename = function() {
+  return (
+    gulp
+      .src(paths.cu)
+      .pipe(renameExt(".jpeg", ".jpg"))
+      // .pipe(debug({ title: "List of files: " }))
+      .pipe(gulp.dest(paths.cuBackup))
+  );
 };
 
-pipes.filterByExt = function(ext) {
-  return filter(`**/*${ext}`, { dot: true });
-};
-
-pipes.basicFilter = async function() {
-  return await gulp
-    .src(paths.cu)
-    .pipe(pipes.renameExt(".jpeg", ".jpg"))
-    .pipe(pipes.filterByExt(".jpg"))
-    // .pipe(debug({ title: "test: " }))
-    .pipe(gulp.dest(paths.outputToBasicFilter));
-};
-
-pipes.filterWrongNames = async function() {
-  const regex = /\d{4}-\d{2}-\d{2}\s\d{2}\.\d{2}\.\d{2}(-\d)?(\s)?(-)?(\s)?(.+)?\.jpg/;
-  return await gulp
-    // .src(paths.basicFilter, { base: "./" })
-    .src(paths.basicFilter)
-    .pipe(pipes.fotoFilenameFilter(regex))
-    .pipe(gulp.dest(paths.outputToWrong));
-};
-
-pipes.fotoFilenameFilter = function(regex) {
-  const filterFiles = file => !regex.test(file.path);
-  return filter(filterFiles, { dot: true });
-};
-
-// clear tasks
-gulp.task("clear:all", tasks.clearAll);
-gulp.task("clear:wrong", tasks.clearWrong);
-gulp.task("clear:basic", tasks.clearBasic);
-
-// main gulp tasks
-gulp.task("basicFilter", pipes.basicFilter);
-gulp.task("default", pipes.filterWrongNames);
+gulp.task("rename", pipes.rename);
+gulp.task("cleanDev", cleanDir_cuBackup);
+gulp.task("default", gulp.series("rename"));
