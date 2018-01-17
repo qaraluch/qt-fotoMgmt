9d56e317a0494bee7e281298076c00515d8dcda7
qaraluch <prezenciq@gmail.com>
2018-01-17 11:23:13 +0100

    Simplified logging for normalize photo names fn

--- fns/normalize-photo-names.js
+++ fns/normalize-photo-names.js
@@ -11,7 +11,13 @@ const through = require("through2");
 
 const regex = /(\d{4}-\d{2}-\d{2}\s\d{2}\.\d{2}\.\d{2})(-\d)?(.+)?/;
 
-module.exports = function normalizePhotoNames() {
+module.exports = function normalizePhotoNames(options) {
+  const defaultOptions = {
+    minimalLog: true
+  };
+  const endOptions = Object.assign({}, defaultOptions, options);
+  const { minimalLog } = endOptions;
+
   let stream = through.obj((file, enc, cb) => {
     const extension = path.extname(file.path);
     // const basename = path.relative(file.base, file.path);
@@ -20,9 +26,11 @@ module.exports = function normalizePhotoNames() {
     const date = regResults[1];
     const version = regResults[2];
     const comment = regResults[3];
-    log(`      -  date:            ${chalk.reset(date)}`);
-    log(`      -  version:         ${chalk.reset(version || "n/a")}`);
-    log(`      -  comment:         ${chalk.reset(comment || "n/a")}`);
+    minimalLog || log(`      -  date:            ${chalk.reset(date)}`);
+    minimalLog ||
+      log(`      -  version:         ${chalk.reset(version || "n/a")}`);
+    minimalLog ||
+      log(`      -  comment:         ${chalk.reset(comment || "n/a")}`);
 
     let newFile = file;
     let newVersion = version || "-0";
